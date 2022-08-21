fdfc8619760bb2a84cad2c1699f247d6dcb70f9d
AirSupan <airsupansa23@gmail.com>
2022-08-21 13:28:16 +0700

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'card_demo.dart';
+import 'contact.dart';
+import 'home.dart';
+import 'list_tile_view.dart';
+import 'list_view_builder.dart';
+import 'list_view_demo.dart';
+import 'list_view_menu.dart';
+import 'list_view_separate.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); //1.constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ListTilePage(
+
+      ),
+    );
+  }
+}
